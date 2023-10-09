f2fe3d957b26a9e0aa4565d183c512388e2bda77
AndreiSalagean <andreysalagean16@gmail.com>
2023-10-09 11:52:05 +0300

    seminar notes in idk.py

--- archive/src_2022_2023/assignments/idk.py
+++ archive/src_2022_2023/assignments/idk.py
@@ -1,3 +1,6 @@
+from random import randint
+from math import gcd
+
 def create_q(num: int ,den: int) ->dict:
     """
 
@@ -5,7 +8,8 @@ def create_q(num: int ,den: int) ->dict:
     :param den:
     :return:
     """
-    return {"num": num , "den": den}
+    g=gcd(num,den)
+    return {"num": num//g , "den": den//g}
 
 def to_str(q:dict)->str:
     """
@@ -26,3 +30,35 @@ for i in range(0,len(data)):
 
 for q in data:
     print(to_str(q))
+
+while True:
+    print("1.add a number")
+    print("2.Display numbers")
+    print("3.Add a Generated random number")
+    print("0.exit")
+
+
+    option=input(">")
+
+    if option=="1":
+        number_str=input("Input number ")
+        token=number_str.split("/")
+        numerator=int(token[0])
+        denominator=int(token[1])
+        data.append(create_q(numerator,denominator))
+        pass
+    elif option=="2":
+        print("The list is : ")
+        for q in data:
+            print(to_str(q),end=', ')
+
+        print("\n")
+        pass
+    elif option=="3":
+        numerator=randint(-10,10)
+        denominator=randint(1,10)
+        data.append(create_q(numerator,denominator))
+
+    elif option=="0":
+        print("bye")
+        break
